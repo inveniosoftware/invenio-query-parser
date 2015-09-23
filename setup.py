c36c7aef7648b0f723e5d484081914e62e715c81
Jiri Kuncar <jiri.kuncar@cern.ch>
2015-09-23 09:38:54 +0200

    installation: Python 3.5 classifier addition
    * BETTER Adds Python 3.5 classifier and enables it in test matrix.
    
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2'
