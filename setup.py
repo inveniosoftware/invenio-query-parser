21003ce6fa124fa9a67e57aa62ba8474ea6f0aee
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-04-12 16:58:11 +0200

    installation: pytest-runner>2.7.0
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- setup.py
+++ setup.py
@@ -28,33 +28,7 @@ import re
 import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
-
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from ConfigParser import ConfigParser
-        except ImportError:
-            from configparser import ConfigParser
-        config = ConfigParser()
-        config.read('pytest.ini')
-        self.pytest_args = config.get('pytest', 'addopts').split(' ')
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 # Get the version string.  Cannot be done with import!
 with open(os.path.join('invenio_query_parser', 'version.py'), 'rt') as f:
@@ -69,6 +43,7 @@ tests_require = [
     'py>=1.4.30',
     'pytest-cov>=2.1.0',
     'pytest-pep8>=1.0.6',
+    'pytest-runner>=2.7.0',
     'pytest>=2.8.0',
 ]
 
@@ -94,7 +69,6 @@ setup(
         'tests': tests_require,
     },
     tests_require=tests_require,
-    cmdclass={'test': PyTest},
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
