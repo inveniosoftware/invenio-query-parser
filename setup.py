2b137fc6d7d2780bd91dfe18c0b710ad318c213d
Alizee Pace <alizee.pace@gmail.com>
2016-08-24 11:53:19 +0200

    installation: removal of Python versions
    Signed-off-by: Alizee Pace <alizee.pace@gmail.com>

--- setup.py
+++ setup.py
@@ -80,9 +80,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2'
